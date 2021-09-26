6b77b7c30daf5a325e5f4abe9161af2264f3a39d
yingfengingrid <90076441+yingfengingrid@users.noreply.github.com>
2021-09-25 23:36:51 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-sudo apt-get install python3
-sudo apt-get install python3-pip
+sudo yum install epel-release
+sudo yum install python38
+curl -O https://bootstrap.pypa.io/get-pip.py
+sudo /usr/bin/python3.4 get-pip.py
 pip install -r requirement.txt
-
